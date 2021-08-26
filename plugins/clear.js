c2574a95ac35373061f5a6a4811e1db5680041a2
jesonpro <88507137+jesonpro@users.noreply.github.com>
2021-08-26 12:21:35 +0530

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
 let {MessageType, GroupSettingChange, ChatModification, WAConnectionTest} = require('@adiwajshing/baileys');
 let WhatsAlexa = require('../events');
 let Config = require('../config');
